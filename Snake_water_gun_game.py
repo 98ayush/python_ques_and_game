bb04f3a09b5e201f80493dce93043c114ca8f5f2
Ayush Agrawal <ayushagrawal17111998@gmail.com>
2020-07-25 03:32:00 +0530

    Update Snake_water_gun_game.py

--- Snake_water_gun_game.py
+++ Snake_water_gun_game.py
@@ -1,56 +1,52 @@
 import random
-user_time=0
-comp_time=0
-choice=1
-while(choice<=9):
+user_time = 0
+comp_time = 0
+choice = 1
+while (choice <= 9):
     comp = ["snake", "water", "gun"]
     print("let computer choose his choice")
     comp_choose = random.choice(comp)
+
 
     print("now its your turn")
     print("lets choose from them\n"
           "1.snake\n 2.water\n 3.gun")
     user_choose = input()
+    print("computer choose ", comp_choose)
     print("you choose", user_choose)
     if user_choose == "snake" and comp_choose == "snake":
         print("tie")
-        comp_time += 1
-        user_time += 1
-    if user_choose == "water" and comp_choose == "water":
+    elif user_choose == "water" and comp_choose == "water":
         print("tie")
-        comp_time += 1
-        user_time += 1
-    if user_choose == "gun" and comp_choose == "gun":
+    elif user_choose == "gun" and comp_choose == "gun":
         print("tie")
-        comp_time += 1
-        user_time += 1
-    if user_choose == "snake" and comp_choose == "gun":
+    elif user_choose == "snake" and comp_choose == "gun":
         print("computer win")
         comp_time += 1
-    if user_choose == "gun" and comp_choose == "snake":
+    elif user_choose == "gun" and comp_choose == "snake":
         print("user win")
         user_time += 1
-    if user_choose == "gun" and comp_choose == "water":
+    elif user_choose == "gun" and comp_choose == "water":
         print("computer win")
         comp_time += 1
-    if user_choose == "water" and comp_choose == "gun":
+    elif user_choose == "water" and comp_choose == "gun":
         print("user win")
         user_time += 1
-    if user_choose == "snake" and comp_choose == "water":
+    elif user_choose == "snake" and comp_choose == "water":
         print("computer win")
         comp_time += 1
-    if user_choose == "water" and comp_choose == "snake":
+    elif user_choose == "water" and comp_choose == "snake":
         print("user win")
         user_time += 1
     else:
         print("invalid")
-    print("choices left ",9-choice)
+    print("choices left ", 9 - choice)
     choice = choice + 1
-if(choice>9):
+if (choice > 9):
     print("time limit exceeds")
-print(user_time," times user won")
-print(comp_time," times computer won")
-if user_time>comp_time:
+print(user_time, " times user won")
+print(comp_time, " times computer won")
+if user_time > comp_time:
     print("finally user win the game")
 else:
     print("finally computer win the game")
